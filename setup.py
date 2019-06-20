e5de7e556e77f42fe3a837df7cbc37c4cddcf1a5
N.K <clax1412@aumc.ac.kr>
2019-06-20 17:24:54 +0900

    Add License and require python version...

--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@ from setuptools import setup
 setup(
     name='PLSP',
     version=1.0,
+    license='MIT',
     maintainer='Neon K.I.D<Kwang Soo Jeong>',
     maintainer_email='contact@neonkid.xyz',
     description='Large size Pathology Image Processing Module',
-    keywords=['Pathology Image', 'SVS', 'CZI', 'Large-size'],
-    url='https://github.com/NEONKID/PILP',
+    keywords=['Pathology Image', 'SVS', 'PLSP', 'Large-size'],
+    url='https://github.com/NEONKID/PLSP',
     install_requires=[
         'openslide-python',
         'numpy',
         'pyvips',
         'czifile'
     ],
+    python_requires = '>=3.5',
     zip_safe=True
 )
